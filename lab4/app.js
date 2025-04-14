28b141d5e32a65dd8edef0128bcb9fe745cb912e
Illia Borovyk <borovyk.illia@lll.kpi.ua>
2025-04-14 11:16:13 +0300

    database creation and connection

---
+++ lab4/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Налаштування EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware для статичних файлів
+app.use('/stylesheets', express.static(path.join(__dirname, 'public/stylesheets')));
+
+// Роутер
+const router = require('./routes/index');
+app.use('/', router);
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Сервер запущено на порті ${PORT}`));
